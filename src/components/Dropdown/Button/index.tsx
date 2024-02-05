d9399df0fae923de665b8d6f4e7e7b1fd67f15a8
unknown <rrdreaming@mail.ru>
2024-02-05 17:40:15 +0300

    :rotating_light: Fix eslint warnings

--- src/components/Dropdown/Button/index.tsx
+++ src/components/Dropdown/Button/index.tsx
@@ -1,24 +1,24 @@
-import React, { useCallback, useContext, memo } from 'react'; 
-import DropdownContext from '../../DropdownContext'; 
- 
-import './index.scss' 
- 
-const DropdownButton = () => { 
-  const { state, setState } = useContext(DropdownContext); 
-  const { label } = state; 
- 
-  const handleChangeOpened = useCallback(() => { 
-    setState((prev) => ({...prev, opened: !prev.opened})) 
-  },[setState]) 
-   
-  return ( 
-    <button  
-      onClick={handleChangeOpened} 
-      className='dropdown__button' 
-    > 
-      {label} 
-    </button> 
-  ) 
-} 
- 
-export default memo(DropdownButton);
+import React, { useCallback, useContext, memo } from 'react'
+import DropdownContext from '../../DropdownContext'
+
+import './index.scss'
+
+const DropdownButton = () => {
+  const { state, setState } = useContext(DropdownContext)
+  const { label } = state
+
+  const handleChangeOpened = useCallback(() => {
+    setState((prev) => ({ ...prev, opened: !prev.opened }))
+  }, [setState])
+
+  return (
+    <button
+      onClick={handleChangeOpened}
+      className='dropdown__button'
+    >
+      {label}
+    </button>
+  )
+}
+
+export default memo(DropdownButton)
